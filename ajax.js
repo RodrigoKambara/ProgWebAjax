c4216cf942fe62be5290c2d462c8dbbbe95bb765
Rodrigo Kambara <50970739+RodrigoKambara@users.noreply.github.com>
2020-05-21 23:09:26 -0300

    Add files via upload

---
+++ ajax.js
@@ -0,0 +1,24 @@
+var produtos = '';
+  $.ajax({
+  url: "https://server-node-example.herokuapp.com/promotios-hub",
+  type:'GET',
+  datatype:'JSON'
+  }).done(function(data) {
+   
+    $.each(data, function(i, campo){
+    produtos += `<div class="col-md-3">
+    <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+    <div class="card-header"></div>
+    <div class="card-body">
+    <h5 class="card-title">${campo.valor}</h5>
+    <p class="card-text">${campo.nome}</p>
+    </div>
+    </div>
+    </div>`;
+  });
+  $(".produtos").append(produtos);
+  });
+
+
+  
+
